bcacb7f3769f01a90fc3877c1fe0684f4c62325a
aleeexk <53943917+aleeexk@users.noreply.github.com>
2021-12-21 11:04:55 +0700

    new script

---
+++ process_data/change_velocity_field.py
@@ -0,0 +1,62 @@
+import os
+import numpy as np
+from ast import literal_eval
+
+path = '/test'
+
+
+def read_vel(path, filename):
+    v = []
+    cur_dir = os.path.join(path, filename)
+    if os.path.isdir(cur_dir):
+        with open(cur_dir + '/U', 'r') as f:
+            m = 0
+            for line in f:
+                if m > 2 and line.strip() != ')':
+                    vect = line.strip()
+                    vect = vect.replace(' ', ',')
+                    vect = np.array(literal_eval(vect))
+                    v.append(vect)
+                m += 1
+            v = np.array(v)
+    return v
+
+
+def average_velocity(path):
+    i = 0
+    v_full = []
+    for filename in list(reversed(os.listdir(path))):
+        v_full.append(read_vel(path, filename))
+        i += 1
+    v_sum = v_full[1]
+    for j in range(2, i, 1):
+        v_sum += v_full[j]
+    return v_sum / 31600
+
+
+def write_new_vel(v, v_mean, path):
+    for filename in list(reversed(os.listdir(path))):
+        cur_dir = os.path.join(path, filename)
+        v_new = v - v_mean
+        with open(cur_dir + '/U', 'w') as f:
+            f.write('\n')
+            f.write('31600\n')
+            f.write('(\n')
+            for i in range(len(v_new)):
+                vect = str(v_new[i]).replace('[', '(')
+                vect = vect.replace(']', ')')
+                vect = vect.replace(',', ' ') + '\n'
+                f.write(vect)
+            f.write(')\n')
+
+
+def main():
+    # np.set_printoptions(threshold=sys.maxsize)
+    v_mean = average_velocity(path)
+    for filename in os.listdir(path):
+        v = (read_vel(path, filename))
+        write_new_vel(v, v_mean, path)
+
+
+if __name__ == '__main__':
+    main()
