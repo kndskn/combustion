6ebe869392108dd8fe28839d79568864df35a4cf
aleeexk <53943917+aleeexk@users.noreply.github.com>
2021-12-21 23:19:01 +0700

    get back previous drawning of plots

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 from process_data import average, parameters
-from process_data.drawning import make_plot
+# from process_data.drawning import make_plot
+from process_data.drawning import old_make_plot
+
 
 input_file = 'model_data/isotherm_structed_after_rans/structed_rans.csv'
 out_f = 'model_data/t_i_average_data_to_2d_matplotlib.tsv'
@@ -45,5 +47,5 @@ if __name__ == '__main__':
         w_exp = 'exp_data/cSwB1_ns_z' + str(parameters.z_label[i]) + '_W_MeanAndRMS.txt'
         uv_exp = 'exp_data/cSwB1_ns_z' + str(parameters.z_label[i]) + '_UV_MeanAndRMS.txt'
         zz = parameters.z_[i]
-        make_plot.write_points_and_made_plot(make_plot.fill_array(output_file, w_exp, uv_exp, zz), LES_mean,
+        old_make_plot.write_points_and_made_plot(old_make_plot.fill_array(output_file, w_exp, uv_exp, zz), LES_mean,
                                              EXP_mean, LES_rms, EXP_rms, MODEL, LIMITS_MEAN, LIMITS_RMS, FIG_NAME)
